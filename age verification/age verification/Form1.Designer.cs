7128f8b75be90e863aee3a911a23f742d7393da6
mchalupa3 <mchalupa3@gmail.com>
2018-01-29 15:01:33 -0600

    udpate

---
+++ age verification/age verification/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace age_verification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEnterAge = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblOtherResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEnterAge
+            // 
+            this.txtEnterAge.Location = new System.Drawing.Point(152, 61);
+            this.txtEnterAge.Name = "txtEnterAge";
+            this.txtEnterAge.Size = new System.Drawing.Size(100, 20);
+            this.txtEnterAge.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Whats your age?";
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResult.Location = new System.Drawing.Point(79, 116);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(206, 48);
+            this.lblResult.TabIndex = 2;
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(79, 256);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 3;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(210, 256);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // lblOtherResult
+            // 
+            this.lblOtherResult.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblOtherResult.Location = new System.Drawing.Point(79, 180);
+            this.lblOtherResult.Name = "lblOtherResult";
+            this.lblOtherResult.Size = new System.Drawing.Size(206, 48);
+            this.lblOtherResult.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 333);
+            this.Controls.Add(this.lblOtherResult);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtEnterAge);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEnterAge;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblOtherResult;
+    }
+}
+
---
+++ age verification/age verification/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace age_verification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            int ageInput;
+
+            ageInput = Convert.ToInt32(txtEnterAge.Text);
+
+            if (ageInput <= 18)
+            {
+                lblResult.Text = "You are a miner";
+            }
+               
+            if (ageInput >= 14 )
+                {       
+                    lblResult.Text = "You are in High School and you smell";
+                }
+                else 
+                { 
+                    lblOtherResult.Text = "you are too small to insult";
+                }
+            if (ageInput >= 21 && ageInput < 19)
+            {
+                lblResult.Text = "Congrats you no longer smell";
+                if (ageInput == 21)
+                {
+                    lblOtherResult.Text = "You are old enough to drink but probably should not";
+                }
+
+            }
+            if (ageInput > 21 && ageInput < 35)
+            {
+                lblResult.Text = "you are having a quater life crisis";
+                if (ageInput == 30)
+                {
+                    lblOtherResult.Text = "HAHAHA you turned 30!";
+
+                }
+            }
+            if (ageInput > 36 && ageInput < 45)
+            {
+                lblResult.Text = "You are getting pretty old.";
+
+            }
+            if (ageInput > 46 && ageInput < 55)
+            {
+                lblResult.Text = "Yep you are old now.";
+            }
+            if (ageInput > 56 && ageInput <=75)
+            {
+                lblResult.Text = "youre really old now";
+
+            }
+            if (ageInput > 75)
+            {
+                lblResult.Text = "congrats on making it this far";
+
+
+            }
+           
+        }
+    }
+}
